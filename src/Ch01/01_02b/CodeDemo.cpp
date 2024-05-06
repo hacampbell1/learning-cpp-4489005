9db052843cea980ff153ff5b50003bc8f5c9eea9
hacampbell1 <140438534+hacampbell1@users.noreply.github.com>
2024-05-06 19:49:32 +0000

    Outputting words in terminal

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,22 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+// First must include all the libararies you want to use
+
+// IO Stream helps recieve text from keyboard and helps print things on a screen
+#include <iostream>
+
+// Starting function block with "int" to represent initialize
+
+// start and end block of code with curly brackets
+int main(){
+    //STD = standard libary
+    // cout = character output --> telling it to output it into the terminal
+    std::cout << "Hi There" << std::endl;
+
+    // helps with debugging, if you get the done message it doesn't have a bug 
+    return(0);
+
+
+}
