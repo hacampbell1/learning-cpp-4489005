15bf04b45b85f85a66b1bd08e2ed1806a31748c0
hacampbell1 <140438534+hacampbell1@users.noreply.github.com>
2024-05-13 16:09:06 +0000

    arrays and directives

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -2,10 +2,30 @@
 // Exercise 02_06
 // Preprocessor directives, by Eduardo Corpeño 
 
+// this is considered a directive
+// tells the preprocessor what to add or remove in the code
+// all directives start with the hash character
 #include <iostream>
-
+#include <string>
+#include <cstdint>
+// everytime it finds CAPACITY it replace it with 5000 not matter what
+#define CAPACITY 5000
+#define DEBUG
 int main(){
-    
+    int32_t large = CAPACITY;
+    // adding the u at the begining makes it an unsigned integer so will have to define the type
+    uint8_t small = 37;
+    // this is useful for debugging it is not the same as the if else looping, you must define the debug error before doing this so it will look for the specific thing and hit that ifdef if done. If you just define debug it will display the message
+#ifdef DEBUG
+    std::cout << "About to perfrom the addition" << std::endl;
+#endif
+    // same thing as python
+    large += small;
+    //large = large + small;
+
+    std::cout <<"this is the large interger " << large << std::endl;
+    // this is not the loop but asks if a 
+   
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,7 +4,26 @@
 
 #include <iostream>
 
+// this is a macro so it has no scope
+// Do not put a semi colon at the end of this line
+//#define AGE_LENGTH 4
 int main(){
+    // another way to define a constant
+    const size_t AGE_LENGTH = 4;
+    // This assigns the data type of the array as int and the number in between the brackets specifies how many values we can place within the array since it cannot be changed
+    // can also make a constant using a macro
+    int age[AGE_LENGTH];
+    // this is techincally doubles because you don't have an f but because you define the array as a float it will make them a float
+    float temperature[]= {1.4,1.5,1.6};
+
+    //assign values in the array
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 15;
+    age[3] = 19;
+
+    std::cout << age[0]<< age[1]<<age[2]<<age[3]<< std::endl;
+    std::cout << temperature[0]<< temperature[1]<<temperature[2]<< std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
