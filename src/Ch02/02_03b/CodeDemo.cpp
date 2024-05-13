b9710d293486744d6d86764716116ad692f955aa
hacampbell1 <140438534+hacampbell1@users.noreply.github.com>
2024-05-13 15:40:05 +0000

    learning variable types

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,25 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+// Just like python can declare multiple variables in a line
 
+// declaring outside of a function will allow the variables to be a global variable
+int a,b =5;
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    // this is making myflag a booling variable but we have not assigned a value to it
+    bool myflag;
+    a =7;
+    myflag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "myflag = " << myflag << std::endl;
+    std::cout << a + b + myflag << std::endl;
+
+    // this will show the binary number so gives you a huge number
+    unsigned int positive;
+    positive = b-a;
+    std::cout << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,10 +3,30 @@
 // Type inference with auto, by Eduardo Corpeño 
 
 #include <iostream>
+// helps you declare variable any types
 #include <typeinfo>
 
 int main(){
     
+    auto a = 8;
+    // trying to make more than 32 bits
+    auto b = 123456789101;
+    //float
+    auto c = 3.14f;
+    //double since it isn't have an f
+    auto d = 3.14;
+    auto e = true;
+    //char type
+    auto f = 'd';
+    //string type
+    auto g = "c plus";
+    std::cout << "The type of a is " << typeid(a).name()<< std::endl;
+    std::cout << "The type of b is " << typeid(b).name()<< std::endl;
+    std::cout << "The type of c is " << typeid(c).name()<< std::endl;
+    std::cout << "The type of d is " << typeid(d).name()<< std::endl;
+    std::cout << "The type of e is " << typeid(e).name()<< std::endl;
+    std::cout << "The type of f is " << typeid(f).name()<< std::endl;
+    std::cout << "The type of g is " << typeid(g).name()<< std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
